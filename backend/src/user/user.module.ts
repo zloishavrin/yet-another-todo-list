5f7a16e5eadddf6fb597ea8a67faec6be60c9928
zloishvarin <nexxxusoffical@gmail.com>
2024-12-04 08:28:45 +0300

    B3 - Users Endpoints

---
+++ backend/src/user/user.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from '@db/user.schema';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: User.name,
+        schema: UserSchema
+      }
+    ]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: 60*60*24*30
+      }
+    })
+  ],
+  controllers: [UserController],
+  providers: [UserService]
+})
+export class UserModule {}
---
+++ backend/src/user/user.types.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsMongoId, IsNotEmpty } from "class-validator";
+import { Types } from "mongoose";
+
+export class UserResponseDto {
+  @ApiProperty({
+    example: '832198jdskfjsdf881238',
+    description: 'ID пользователя'
+  })
+  @IsMongoId()
+  _id: Types.ObjectId;
+
+  @ApiProperty({
+    example: 'login123',
+    description: 'Логин пользователя'
+  })
+  @IsNotEmpty()
+  login: string;
+}
